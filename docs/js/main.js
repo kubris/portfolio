c498b45159e94f08a65ef054413558bc2b6138f4
Andrey Martynov <kubris@mail.ru>
2020-12-13 15:20:31 +0300

    Initial commit of Portfolio project

---
+++ docs/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+        $("a[href^='#']").click( function () {
+                var _href = $(this).attr("href");
+                $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+                return false;
+        });
+});
