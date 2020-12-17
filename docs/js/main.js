dc80f2b95d94c4938f6a27b0b0a20b22375b9508
Andrey Martynov <kubris@mail.ru>
2020-12-18 00:15:28 +0300

    Created header with media-queries

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,4 @@
+// link-delay
 $(function(){
         $("a[href^='#']").click( function () {
                 var _href = $(this).attr("href");
@@ -6,7 +7,32 @@ $(function(){
         });
 });
 
-const toggleMenu = document.querySelector('.toggle-menu'); 
+// mobile-menu
+const toggleMenu = document.querySelector('.toggle-menu'); // gamburger icon 
+const mobileMenu = document.querySelector('.mobile-menu');  // mobile menu list
+const overlay = document.querySelector('#overlay');  // overlay block
+const tagBody = document.body;  // tag body
+
+// gamburger click
 toggleMenu.addEventListener('click', function() {
 	this.classList.toggle('active');
+	mobileMenu.classList.toggle('active');
+	overlay.classList.toggle('active');
+	tagBody.classList.toggle('noscroll');
+});
+
+// mobile menu click
+mobileMenu.addEventListener('click', function() {
+	this.classList.remove('active');
+	toggleMenu.classList.remove('active');
+	overlay.classList.remove('active');
+	tagBody.classList.remove('noscroll');
+});
+
+// overlay click 
+overlay.addEventListener('click', function() {
+	this.classList.remove('active');
+	toggleMenu.classList.remove('active');
+	mobileMenu.classList.remove('active');
+	tagBody.classList.remove('noscroll');
 });
