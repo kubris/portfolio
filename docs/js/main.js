8e5e90a39cb25c6b4d288f7bf8cde64373d4e621
Andrey Martynov <kubris@mail.ru>
2020-12-24 14:07:58 +0300

    Created timeline-section with pixel-perfect. Non-universal solution

--- docs/js/main.js
+++ docs/js/main.js
@@ -46,6 +46,19 @@ overlay.addEventListener('click', function() {
         classNames: {
             block: ""
         }
-    });
+	});
+	
+	// Подключение точек пагинации справа page-nav
+	$('#page-nav').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'swing',
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
 
 });
