85b20e075058a0f5c3516f065c621c7b00582de5
Andrey Martynov <kubris@mail.ru>
2021-01-05 19:27:36 +0300

    Fixed contacts-form and finished PxPr

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,64 +1,122 @@
 $(document).ready(function () {
+	// ==== link-relocation-delay ====
+	$(function () {
+		$("a[href^='#']").click(function () {
+			var _href = $(this).attr("href");
+			$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+			return false;
+		});
+	});
 
-// ==== link-relocation-delay ====
-$(function(){
-        $("a[href^='#']").click( function () {
-                var _href = $(this).attr("href");
-                $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-                return false;
-        });
-});
+	// ==== mobile-menu ====================
+	const toggleMenu = document.querySelector(".toggle-menu"); // gamburger icon
+	const mobileMenu = document.querySelector(".mobile-menu"); // mobile menu list
+	const overlay = document.querySelector("#overlay"); // overlay block
+	const tagBody = document.body; // tag body
+	const mobileLinks = document.querySelectorAll(".mobile-menu__link"); 
 
-// ==== mobile-menu ====
-const toggleMenu = document.querySelector('.toggle-menu'); // gamburger icon 
-const mobileMenu = document.querySelector('.mobile-menu');  // mobile menu list
-const overlay = document.querySelector('#overlay');  // overlay block
-const tagBody = document.body;  // tag body
+	// ==== gamburger click ====
+	toggleMenu.addEventListener("click", function () {
+		this.classList.toggle("toggle-menu--active");
+		mobileMenu.classList.toggle("active");
+		overlay.classList.toggle("active");
+		tagBody.classList.toggle("noscroll");
+	});
 
-// ==== gamburger click ====
-toggleMenu.addEventListener('click', function() {
-	this.classList.toggle('toggle-menu--active');
-	mobileMenu.classList.toggle('active');
-	overlay.classList.toggle('active');
-	tagBody.classList.toggle('noscroll');
-});
+	// ==== mobile menu click ====
+	mobileMenu.addEventListener("click", function () {
+		this.classList.remove("active");
+		toggleMenu.classList.remove("toggle-menu--active");
+		overlay.classList.remove("active");
+		tagBody.classList.remove("noscroll");
+	});
 
-// ==== mobile menu click ====
-mobileMenu.addEventListener('click', function() {
-	this.classList.remove('active');
-	toggleMenu.classList.remove('toggle-menu--active');
-	overlay.classList.remove('active');
-	tagBody.classList.remove('noscroll');
-});
-
-// ==== overlay click  ====
-overlay.addEventListener('click', function() {
-	this.classList.remove('active');
-	toggleMenu.classList.remove('toggle-menu--active');
-	mobileMenu.classList.remove('active');
-	tagBody.classList.remove('noscroll');
-});
-
-// ==== mixitup3 ====	
-    let containerEl = document.querySelector('#works-block');
-
-    let mixer = mixitup(containerEl, {
-        classNames: {
-            block: ""
-        }
+	// ==== overlay click  ====
+	overlay.addEventListener("click", function () {
+		this.classList.remove("active");
+		toggleMenu.classList.remove("toggle-menu--active");
+		mobileMenu.classList.remove("active");
+		tagBody.classList.remove("noscroll");
 	});
 	
-	// Подключение точек пагинации справа page-nav
-	$('#page-nav').onePageNav({
-		currentClass: 'active',
+	// ==== Link click  ====
+	for(let item of mobileLinks) {
+		item.addEventListener("click", function () {
+			overlay.classList.remove("active");
+			toggleMenu.classList.remove("toggle-menu--active");
+			mobileMenu.classList.remove("active");
+			tagBody.classList.remove("noscroll");
+			});
+	};
+	// ==== end of a mobile-menu ====================
+
+	// ==== mixitup3 ====
+	let containerEl = document.querySelector("#works-block");
+
+	let mixer = mixitup(containerEl, {
+		classNames: {
+			block: "",
+		},
+	});
+
+	// Подключение точек пагинации page-nav
+	$("#page-nav").onePageNav({
+		currentClass: "active",
 		changeHash: false,
 		scrollSpeed: 750,
 		scrollThreshold: 0.5,
-		filter: '',
-		easing: 'swing',
+		filter: "",
+		easing: "swing",
 		begin: function () {},
 		end: function () {},
-		scrollChange: function ($currentListItem) {}
+		scrollChange: function ($currentListItem) {},
 	});
 
+	// FORM VALIDATE
+
+	$("#contacts-form").validate({
+		rules: {
+			email: {
+				required: true,
+				email: true,
+			},
+			subject: {
+				required: true,
+			},
+			message: {
+				required: true,
+			},
+		},
+		messages: {
+			email: {
+				required: "Введите email",
+				email: "Отсутствует символ @",
+			},
+			subject: {
+				required: "Введите тему сообщения",
+			},
+			message: {
+				required: "Введите текст сообщения",
+			},
+		},
+	});
+
+	// FORM - placeholder
+	const formItems = document.querySelectorAll('.contacts-form__item')
+	for(let item of formItems) {
+		const thisParent = item.closest('.contacts-form__cell');
+		const thisPlaceholder = thisParent.querySelector('.contacts-form__label');
+
+		item.addEventListener('focus', function() {
+			thisPlaceholder.classList.add('label-active');
+		});
+
+		item.addEventListener('blur', function() {
+			if (item.value.length > 0) {
+				thisPlaceholder.classList.add('label-active');
+			} else {
+				thisPlaceholder.classList.remove('label-active');
+			}
+		});
+	}
 });
