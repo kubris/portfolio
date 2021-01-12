750494a524e0914fac5d73d4f8b25629eeec797d
Andrey Martynov <kubris@mail.ru>
2021-01-12 11:58:29 +0300

    Fixed portfolio script

--- docs/js/main.js
+++ docs/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 	const mobileMenu = document.querySelector(".mobile-menu"); // mobile menu list
 	const overlay = document.querySelector("#overlay"); // overlay block
 	const tagBody = document.body; // tag body
-	const mobileLinks = document.querySelectorAll(".mobile-menu__link"); 
+	const mobileLinks = document.querySelectorAll(".mobile-menu__link");
 
 	// ==== gamburger click ====
 	toggleMenu.addEventListener("click", function () {
@@ -38,89 +38,96 @@ $(document).ready(function () {
 		mobileMenu.classList.remove("active");
 		tagBody.classList.remove("noscroll");
 	});
-	
+
 	// ==== Link click  ====
-	for(let item of mobileLinks) {
+	for (let item of mobileLinks) {
 		item.addEventListener("click", function () {
 			overlay.classList.remove("active");
 			toggleMenu.classList.remove("toggle-menu--active");
 			mobileMenu.classList.remove("active");
 			tagBody.classList.remove("noscroll");
-			});
-	};
+		});
+	}
 
 	// FORM - placeholder
-	const formItems = document.querySelectorAll('.contacts-form__item')
-	for(let item of formItems) {
-		const thisParent = item.closest('.contacts-form__cell');
-		const thisPlaceholder = thisParent.querySelector('.contacts-form__label');
+	const formItems = document.querySelectorAll(".contacts-form__item");
+	for (let item of formItems) {
+		const thisParent = item.closest(".contacts-form__cell");
+		const thisPlaceholder = thisParent.querySelector(".contacts-form__label");
 
-		item.addEventListener('focus', function() {
-			thisPlaceholder.classList.add('label-active');
+		item.addEventListener("focus", function () {
+			thisPlaceholder.classList.add("label-active");
 		});
 
-		item.addEventListener('blur', function() {
+		item.addEventListener("blur", function () {
 			if (item.value.length > 0) {
-				thisPlaceholder.classList.add('label-active');
+				thisPlaceholder.classList.add("label-active");
 			} else {
-				thisPlaceholder.classList.remove('label-active');
+				thisPlaceholder.classList.remove("label-active");
 			}
 		});
 	}
 	// ==== end of a mobile-menu =====
 
-	// ==== mixitup3 ====
-	let containerEl = document.querySelector('#works-block');
-	let mixer = mixitup(containerEl, {
-		classNames: {
-			block: ""
-		}
-	});
-
-	// add/remove two big cards in portfolio
-	const filterToggles = document.querySelectorAll('.works-buttons__block button');
-	const portfolioBigCards = document.querySelectorAll('.works-card');
-	for (let i = 0; i < filterToggles.length; i++) {
-		filterToggles[i].addEventListener('click', function () {
-			if (i == 0) {
-				for (let j = 0; j < 2; j++) {
-					portfolioBigCards[j].classList.add('works-card--big');
-				}
-			} else {
-				for (let j = 0; j < 2; j++) {
-					portfolioBigCards[j].classList.remove('works-card--big');
-				}
-			}
-		});
-	}
-
 	// show/hide work-cards when the page is loading
 	if ($(window).width() < 1200 && $(window).width() > 767) {
-		$('.works-card.hide-card').hide();
-		$('.works-card__btn').on('click', function() {
-			$('.works-card.hide-card').fadeIn();
+		$(".works-card.hide-card").hide();
+		$(".works-card__btn").on("click", function () {
+			$(".works-card.hide-card").fadeIn();
 			$(this).hide();
 		});
 	} else {
-		$('.works-card.hide-card').fadeIn();
+		$(".works-card.hide-card").fadeIn();
 		$(this).hide();
 	}
 
 	// show/hide work-cards when the page is resizing
-	$(window).on('resize',function() {
-		if($(window).width() < 1200) {
-			$('.works-card.hide-card').hide();
-			$('.works-card__btn').fadeIn();
+	$(window).on("resize", function () {
+		if ($(window).width() < 1200) {
+			$(".works-card.hide-card").hide();
+			$(".works-card__btn").fadeIn();
 
-			$('.works-card__btn').on('click', function() {
-				$('.works-card.hide-card').fadeIn();
-				$(this).css('display', 'none');
+			$(".works-card__btn").on("click", function () {
+				$(".works-card.hide-card").fadeIn();
+				$(this).css("display", "none");
 			});
 		} else {
-			$('.works-card.hide-card').fadeIn();
-			$('.works-card__btn').hide();
+			$(".works-card.hide-card").fadeIn();
+			$(".works-card__btn").hide();
 		}
 	});
+
+	// ==== mixitup3 ====
+	let containerEl = document.querySelector("#works-block");
+	let mixer = mixitup(containerEl, {
+		classNames: {
+			block: "",
+		},
+	});
+
+	// add/remove two big cards in portfolio
+	const filterToggles = document.querySelectorAll(".works-buttons__block button");
+	const portfolioBigCards = document.querySelectorAll(".works-card");
+
+	for (let i = 0; i < filterToggles.length; i++) {
+		filterToggles[i].addEventListener("click", function () {
+			$(".works-card.hide-card").fadeIn();
+			$(".works-card__btn").hide();
+			if (i == 0) {
+				for (let j = 0; j < 2; j++) {
+					portfolioBigCards[j].classList.add("works-card--big");
+				}
+			} else {
+				for (let j = 0; j < 2; j++) {
+					portfolioBigCards[j].classList.remove("works-card--big");	
+				}
+				for (let k = 0; k < filterToggles.length; k++) {
+					console.log(filterToggles.length);
+					console.log(portfolioBigCards[k]);
+				}
+			}
+		});
+	}
 
 	// Create page-nav dots
 	$("#page-nav").onePageNav({
@@ -136,7 +143,7 @@ $(document).ready(function () {
 	});
 
 	// FORM VALIDATE
-	$('#contacts-form').validate({
+	$("#contacts-form").validate({
 		rules: {
 			email: {
 				required: true,
@@ -159,11 +166,11 @@ $(document).ready(function () {
 			},
 			message: {
 				required: "Введите текст сообщения",
-			}
+			},
 		},
-		submitHandler: function(from) {
+		submitHandler: function (from) {
 			ajaxFormSubmit();
-		}
+		},
 	});
 
 	// Send message by contacts-form
@@ -173,22 +180,22 @@ $(document).ready(function () {
 			type: "POST",
 			url: "php/mail.php",
 			data: string,
-			success: function(html) {
+			success: function (html) {
 				$(".contacts-form").slideUp(800);
 				$("#answer").html(html);
-			}
+			},
 		});
 		return false;
 	}
 
 	// Paralax-effect on contacts-form
-	let prxScene = document.querySelector('.contacts');
-	let prxItems = document.querySelectorAll('.move-quote');
-	prxScene.addEventListener('mousemove', function (e) {
+	let prxScene = document.querySelector(".contacts");
+	let prxItems = document.querySelectorAll(".move-quote");
+	prxScene.addEventListener("mousemove", function (e) {
 		let x = e.clientX / window.innerWidth;
 		let y = e.clientY / window.innerHeight;
 		for (let item of prxItems) {
-			item.style.transform = 'translate(-' + x * 35 + 'px, -' + y * 35 + 'px)';
+			item.style.transform = "translate(-" + x * 35 + "px, -" + y * 35 + "px)";
 		}
 	});
-})
+});
